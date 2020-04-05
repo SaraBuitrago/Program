6be6d8d9706cc6dffc1adb40e0e1b030bd4e9e6b
Sara <sm.buitrago@usp.ceu.es>
2020-04-05 12:27:25 +0200

    Primer commit de la practica

--- HarryPotter/src/harrypotter/HarryPotter.java
+++ HarryPotter/src/harrypotter/HarryPotter.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//Tiene que llevar mi nombre en alguna parte sbuitrago
+//El repositorio tiene que ser privado
 package harrypotter;
 
 /**
